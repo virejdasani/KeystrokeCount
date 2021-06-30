30e771e2d84dc7ee1ac81ab31289b179e979a9fb
Virej Dasani <dasanivirej@gmail.com>
2021-06-30 19:38:44 +0530

    Code cleanup 🧹

--- app/index.js
+++ app/index.js
@@ -18,6 +18,8 @@ if (keysPressed === null) {
 // When a key press is detected in the main process this happens
 ipcRenderer.on("keydownEvent", (event, keyCodesPressed) => {
   // We find which key is pressed by comparing the keyCode we get from the main process to the keyNames in keys.js
+  // keyCodesPressed is an array of keyCodes of all the keys pressed. keyCodesPressed[0] gives us the last key pressed
+  // keys is the array in keys.js
   var key = keys.find((key) => key.keyCode === keyCodesPressed[0]);
   console.log(key.keyName);
 });
