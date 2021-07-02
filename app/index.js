12ed21ee9ed85990ce37dffde16564b6650bc934
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-07-02 14:54:23 +0530

    Fixed a JSON.parse bug

--- app/index.js
+++ app/index.js
@@ -9,23 +9,24 @@ var localKeys = localStorage.getItem("localKeys");
 
 // If localKeys doesn't exist in localStorage, we initialize it to the keys array from keys.js
 if (localKeys === null) {
-  localStorage.setItem("localKeys", keys);
+  localStorage.setItem("localKeys", JSON.stringify(keys));
+  console.log("SET ARR");
 }
 
 // When a key press is detected in the main process this happens
 ipcRenderer.on("keydownEvent", (event, keyCodesPressed) => {
   // Set localKeys to "localKeys" from localStorage
-  localKeys = localStorage.getItem("localKeys");
+  localKeys = JSON.parse(localStorage.getItem("localKeys"));
 
   // We loop over all the objects in the keys array (from keys.js)
-  for (var i in keys) {
+  for (var i in localKeys) {
     // If the keyCode in the keys array matches the last pressed key
-    if (keys[i].keyCode == keyCodesPressed[0]) {
+    if (localKeys[i].keyCode == keyCodesPressed[0]) {
       // For that key, the timesClicked in that object is incremented by 1
-      keys[i].timesClicked += 1;
-      // Now update the keys array in localStorage to reflect the +1 in timesClicked
-      localStorage.setItem("localKeys", JSON.stringify(keys));
-      // This gets the keys array from localStorage and logs
+      localKeys[i].timesClicked += 1;
+      // Now update the localKeys array in localStorage to reflect the +1 in timesClicked
+      localStorage.setItem("localKeys", JSON.stringify(localKeys));
+      // This gets the localKeys array from localStorage and logs
       console.log(JSON.parse(localStorage.getItem("localKeys")));
       // Break out of the loop
       break;
@@ -35,11 +36,11 @@ ipcRenderer.on("keydownEvent", (event, keyCodesPressed) => {
 
 // For mouse clicks
 // When left click is detected
-ipcRenderer.on("leftClickEvent", (event, message) => {
-  leftClick.innerHTML = "Number of left clicks: " + message;
+ipcRenderer.on("leftClickEvent", (event, leftClicks) => {
+  leftClick.innerHTML = "Number of left clicks: " + leftClicks;
 });
 
 // When right click is detected
-ipcRenderer.on("rightClickEvent", (event, message) => {
-  rightClick.innerHTML = "Number of right clicks: " + message;
+ipcRenderer.on("rightClickEvent", (event, rightClicks) => {
+  rightClick.innerHTML = "Number of right clicks: " + rightClicks;
 });
