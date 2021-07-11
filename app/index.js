54bbce63f09800b96e44cda0320cb226822012f2
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-07-11 19:24:18 +0530

    Added update functionality

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+var appVersion = "1.0.0";
+
 const { ipcRenderer } = require("electron");
 
 // Get the localKeys from localStorage
@@ -76,3 +78,23 @@ ipcRenderer.on("rightClickEvent", (event, rightClicks) => {
     }
   }
 });
+
+// For app update, if an update is available, the updateAvailable in the RemoteJSON repo will be updated to yes. That will result in the code below being executed
+fetch("https://virejdasani.github.io/RemoteJSON/KeystrokeCount/index.html")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // If update is available, and this version is not the latest one, the update div will no longer be empty. It will have the following HTML
+    if (data.updateAvailable == "yes" && data.latestVersion != appVersion) {
+      document.getElementById("update").innerHTML = `
+                <div id="update">
+                    ${data.updateText}
+                    Download update <!-- (${data.latestVersion}) --> <a href="${data.updateURL}" target="_blank">here</a>
+                </div>
+            `;
+    }
+  })
+  .catch((err) => {
+    // console.log(err)
+  });
